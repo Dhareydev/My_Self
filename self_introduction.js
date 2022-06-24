43b708afd0ca376500c7976ebdf602d8d2816a3b
DhareyDev <Dharexbee21@gmail.com>
2022-06-24 15:39:24 +0100

    My_Self

---
+++ self_introduction.js
@@ -0,0 +1,4 @@
+// Javascript code that prints out your name, height, and country on the screen
+console.log('Name: Bello Emmanuel Oluwadamilare')
+console.log('Height: 180cm');
+console.log('Country: Nigeria');
